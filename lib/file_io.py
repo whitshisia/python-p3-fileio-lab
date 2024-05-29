ee1de85b199248e78400cc58714f231504e95801
whitney-shisia <shisiawhitney215@gmail.com>
2024-05-29 13:41:17 +0300

    py

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+     if not str(file_name).endswith('.txt'):
+        file_name = str(file_name) + '.txt'
+    
+     with open(file_name, 'w') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     if not str(file_name).endswith('.txt'):
+        file_name = str(file_name) + '.txt'
+    
+     with open(file_name, 'a') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = str(file_name) + '.txt'
+    
+    with open(file_name, 'r') as text_file:
+        return text_file.read()
